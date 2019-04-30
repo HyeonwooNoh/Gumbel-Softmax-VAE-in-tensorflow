b4fe72494543422453d1661edb148265819213ea
Hyeonwoo Noh <hyeonwoonoh@gmail.com>
2019-04-30 23:17:37 +0900

    cvae_paper clean - remove unnecessary codes

--- model/cvae_paper.py
+++ model/cvae_paper.py
@@ -3,17 +3,15 @@ import math
 
 from tensorflow.contrib import slim
 
-from util.layer import GaussianLogDensity, GaussianKLD, \
-    GaussianSampleLayer, lrelu, GumbelSampleLayer, GumbelSoftmaxLogDensity
+from util.layer import GaussianKLD, GaussianSampleLayer, lrelu, GumbelSampleLayer
 
 EPS = tf.constant(1e-10)
 
+
 class CVAE(object):
     '''
-      VC-GAN
-    = CVAE-CGAN
-    = Convolutional Variational Auto-encoder
-      with Conditional Generative Adversarial Net
+    Semi-supervised VAE with Gumbel softmax with architecture described in:
+        https://arxiv.org/abs/1611.01144
     '''
     def __init__(self, arch, is_training=False):
         self.arch = arch
@@ -27,26 +25,18 @@ class CVAE(object):
                     name='tau')) + 0.1
 
         self._generate = tf.make_template(
-            'Generator',
-            self._generator)
-        self._discriminate = tf.make_template(
-            'Discriminator',
-            self._discriminator)
+            'Generator', self._generator)
         self._encode = tf.make_template(
-            'Encoder',
-            self._encoder)
+            'Encoder', self._encoder)
         self._classification_weight = tf.make_template(
             'Classifier', self._classifier_weight)
         self._classify_with_weight = tf.make_template(
             'Classifier', self._classifier_with_weight)
-        self._classify_inner = tf.make_template(
-            'Classifier', self._classifier)
 
     def _classify(self, x, is_training=False, weight=None):
         if weight is None:
             weight = self._classification_weight()
         return self._classify_with_weight(x, is_training, weight)
-        #return self._classify_inner(x, is_training=is_training)
 
     def _sanity_check(self):
         for net in ['encoder', 'generator', 'classifier']:
@@ -54,41 +44,20 @@ class CVAE(object):
             assert len(self.arch[net]['output']) == len(self.arch[net]['kernel'])
             assert len(self.arch[net]['output']) == len(self.arch[net]['stride'])
 
-
-    def _unit_embedding(self, n_class, h_dim, scope_name, var_name='y_emb'):
-        with tf.variable_scope(scope_name):
-            embeddings = tf.get_variable(
-                name=var_name,
-                shape=[n_class, h_dim])
-            embeddings = tf.nn.l2_normalize(embeddings, dim=-1, name=var_name+'normalized')
-        return embeddings
-
-
     def _merge(self, var_list, fan_out, l2_reg=1e-6):
         '''
         Note: Don't apply BN on this because 'y'
               tends to be the same inside a batch.
         '''
         x = 0.
-        with slim.arg_scope(
-            [slim.fully_connected],
-            num_outputs=fan_out,
-            weights_regularizer=slim.l2_regularizer(l2_reg),
-            normalizer_fn=None,
-            activation_fn=None):
+        with slim.arg_scope([slim.fully_connected],
+                            num_outputs=fan_out,
+                            weights_regularizer=slim.l2_regularizer(l2_reg),
+                            normalizer_fn=None, activation_fn=None):
             for var in var_list:
                 x = x + slim.fully_connected(var)
         x = slim.bias_add(x)
         return x
-
-
-    def _l2_regularized_embedding(self, n_class, h_dim, scope_name, var_name='y_emb'):
-        with tf.variable_scope(scope_name):
-            embeddings = tf.get_variable(
-                name=var_name,
-                shape=[n_class, h_dim],
-                regularizer=slim.l2_regularizer(1e-6))
-        return embeddings
 
     def _classifier_weight(self):
         n_layer = len(self.arch['classifier']['output'])
@@ -140,48 +109,6 @@ class CVAE(object):
         y_logit = y_logit + weight['fully_connected/biases']
         return y_logit
 
-
-    def _classifier(self, x, is_training):
-        n_layer = len(self.arch['classifier']['output'])
-        subnet = self.arch['classifier']
-
-        with slim.arg_scope(
-            [slim.batch_norm],
-            scale=True, scope='BN',
-            updates_collections=None,
-            # decay=0.9, epsilon=1e-5,  # [TODO] Test these hyper-parameters
-            is_training=is_training):
-            with slim.arg_scope(
-                [slim.conv2d],
-                weights_regularizer=slim.l2_regularizer(subnet['l2-reg']),
-                normalizer_fn=slim.batch_norm,
-                activation_fn=lrelu):
-
-                for i in range(n_layer):
-                    x = slim.conv2d(
-                        x,
-                        subnet['output'][i],
-                        subnet['kernel'][i],
-                        subnet['stride'][i])
-                    tf.summary.image(
-                        'down-sample{:d}'.format(i),
-                        tf.transpose(x[:, :, :, 0:3], [2, 1, 0, 3]))
-
-        x = slim.flatten(x)
-
-        with slim.arg_scope(
-            [slim.fully_connected],
-            num_outputs=self.arch['y_dim'],
-            weights_regularizer=slim.l2_regularizer(subnet['l2-reg']),
-            normalizer_fn=None,
-            activation_fn=None):
-            y_logit = slim.fully_connected(x)
-            # z_mu = slim.fully_connected(x)
-            # z_lv = slim.fully_connected(x)
-        # return z_mu, z_lv
-        return y_logit
-
-
     def _encoder(self, x, y, is_training):
         n_layer = len(self.arch['encoder']['output'])
         subnet = self.arch['encoder']
@@ -198,18 +125,12 @@ class CVAE(object):
 
         x = tf.concat([x, y2x], 3)
 
-        with slim.arg_scope(
-            [slim.batch_norm],
-            scale=True, scope='BN',
-            updates_collections=None,
-            # decay=0.9, epsilon=1e-5,  # [TODO] Test these hyper-parameters
-            is_training=is_training):
-            with slim.arg_scope(
-                [slim.conv2d],
-                weights_regularizer=slim.l2_regularizer(subnet['l2-reg']),
-                normalizer_fn=slim.batch_norm,
-                activation_fn=lrelu):
-
+        with slim.arg_scope([slim.batch_norm], scale=True, scope='BN',
+                            updates_collections=None, is_training=is_training):
+            with slim.arg_scope([slim.conv2d],
+                                weights_regularizer=slim.l2_regularizer(subnet['l2-reg']),
+                                normalizer_fn=slim.batch_norm,
+                                activation_fn=lrelu):
                 for i in range(n_layer):
                     x = slim.conv2d(
                         x,
@@ -222,16 +143,14 @@ class CVAE(object):
 
         x = slim.flatten(x)
 
-        with slim.arg_scope(
-            [slim.fully_connected],
-            num_outputs=self.arch['z_dim'],
-            weights_regularizer=slim.l2_regularizer(subnet['l2-reg']),
-            normalizer_fn=None,
-            activation_fn=None):
+        with slim.arg_scope([slim.fully_connected],
+                            num_outputs=self.arch['z_dim'],
+                            weights_regularizer=slim.l2_regularizer(subnet['l2-reg']),
+                            normalizer_fn=None,
+                            activation_fn=None):
             z_mu = slim.fully_connected(x)
             z_lv = slim.fully_connected(x)
         return z_mu, z_lv
-
 
     def _generator(self, z, y, is_training):
         ''' In this version, we only generate the target, so `y` is useless '''
@@ -242,13 +161,10 @@ class CVAE(object):
         with tf.variable_scope('y_guider'):
             x = self._merge([z, y], subnet['merge_dim'])
             x = lrelu(x)
-        with slim.arg_scope(
-            [slim.batch_norm],
-            scale=True, scope='BN',
-            updates_collections=None,
-            # decay=0.9, epsilon=1e-5,
-            is_training=is_training):
-
+        with slim.arg_scope([slim.batch_norm],
+                            scale=True, scope='BN',
+                            updates_collections=None,
+                            is_training=is_training):
             x = slim.fully_connected(
                 x,
                 h * w * c,
@@ -257,13 +173,11 @@ class CVAE(object):
 
             x = tf.reshape(x, [-1, h, w, c])
 
-            with slim.arg_scope(
-                [slim.conv2d_transpose],
-                weights_regularizer=slim.l2_regularizer(subnet['l2-reg']),
-                normalizer_fn=slim.batch_norm,
-                activation_fn=lrelu):
-
-                for i in range(n_layer -1):
+            with slim.arg_scope([slim.conv2d_transpose],
+                                weights_regularizer=slim.l2_regularizer(subnet['l2-reg']),
+                                normalizer_fn=slim.batch_norm,
+                                activation_fn=lrelu):
+                for i in range(n_layer - 1):
                     x = slim.conv2d_transpose(
                         x,
                         subnet['output'][i],
@@ -286,66 +200,6 @@ class CVAE(object):
                 # x = tf.nn.tanh(logit)
         # return x, logit
         return tf.nn.sigmoid(logit), logit
-
-
-    def _discriminator(self, x, is_training):
-    # def _discriminator(self, x, is_training):
-        ''' Note: In this version, `y` is useless '''
-        subnet = self.arch['discriminator']
-        n_layer = len(subnet['output'])
-
-        # y_dim = self.arch['y_dim']
-        # h, w, _ = self.arch['hwc']
-        # y_emb = self._l2_regularized_embedding(y_dim, h * w, 'y_embedding_disc_in')
-        # y_vec = tf.nn.embedding_lookup(y_emb, y)
-        # y_vec = tf.reshape(y_vec, [-1, h, w, 1])
-
-        intermediate = list()
-        intermediate.append(x)
-
-        # x = tf.concat(3, [x, y_vec])   # inject y into x
-        with slim.arg_scope(
-            [slim.batch_norm],
-            scale=True, scope='BN',
-            updates_collections=None,
-            # decay=0.9, epsilon=1e-5,
-            is_training=is_training):
-            with slim.arg_scope(
-                [slim.conv2d],
-                weights_regularizer=slim.l2_regularizer(subnet['l2-reg']),
-                normalizer_fn=slim.batch_norm,
-                activation_fn=lrelu):
-
-                # Radford: [do] not applying batchnorm to the discriminator input layer
-                x = slim.conv2d(
-                    x,
-                    subnet['output'][0],
-                    subnet['kernel'][0],
-                    subnet['stride'][0],
-                    normalizer_fn=None)
-                intermediate.append(x)
-                for i in range(1, n_layer):
-                    x = slim.conv2d(
-                        x,
-                        subnet['output'][i],
-                        subnet['kernel'][i],
-                        subnet['stride'][i])
-                    intermediate.append(x)
-                    tf.summary.image(
-                        'upsampling{:d}'.format(i),
-                        tf.transpose(x[:, :, :, 0:3], [2, 1, 0, 3]))
-
-        # Don't apply BN for the last layer
-        x = slim.flatten(x)
-        h = slim.flatten(intermediate[subnet['feature_layer'] - 1])
-
-        x = slim.fully_connected(
-            x,
-            1,
-            weights_regularizer=slim.l2_regularizer(subnet['l2-reg']),
-            activation_fn=None)
-
-        return x, h  # no explicit `sigmoid`
 
     def circuit_loop(self, x, y_L=None):
         '''
@@ -393,7 +247,6 @@ class CVAE(object):
             xh_L=xh_L,
             xh_sig_logit_L=xh_sig_logit_L,
         )
-
 
     def loss(self, x_u, x_l, y_l):
         unlabel = self.circuit_loop(x_u)
@@ -494,7 +347,6 @@ class CVAE(object):
                             tf.log(unlabel['y_pred'] + EPS) - tf.log(y_prior)),
                         -1))
 
-
                 # Using Gumbel-Softmax Distribution:
                 #   1. Incorrect because p(y..y) is a scalar-- unless we can get
                 #      the parametic form of the H(Y).
@@ -507,7 +359,6 @@ class CVAE(object):
                 #     tau=self.tau)
                 # # loss['H(y)'] = tf.reduce_mean(- tf.mul(tf.exp(log_qy), log_qy))
                 # loss['H(y)'] = tf.reduce_mean(- log_qy)
-
 
                 # # [TODO] How to define this term? log p(y)
                 # loss['log p(y)'] = - tf.nn.softmax_cross_entropy_with_logits(
@@ -545,10 +396,6 @@ class CVAE(object):
 
                 for k, v in loss.items():
                     tf.summary.scalar('{}'.format(k), v)
-                # tf.summary.histogram('z_lv_t', t['z_lv'])
-
-                # tf.summary.histogram('y_logit', unlabel['y_logit'])
-                # tf.summary.histogram('y', unlabel['y'])
 
         return loss
 
@@ -558,8 +405,7 @@ class CVAE(object):
         y = tf.nn.softmax(y_logit / self.tau)
 
         z_mu, z_lv = self._encode(x, y, is_training=False)
-        # y_logit = self._classify(x, is_training=False)
-        return dict(mu=z_mu, log_var=z_lv, y=y) #, y_logit=y_logit)
+        return dict(mu=z_mu, log_var=z_lv, y=y)
 
     def classify(self, x):
         y_logit = self._classify(x, is_training=False)
@@ -567,29 +413,5 @@ class CVAE(object):
         return y
 
     def decode(self, z, y, tanh=False):
-        # if tanh:
-        #     return self._generate(z, y, is_training=False)
-        # else:
-        #     return self._generate(z, y, is_training=False)
         xh, _ = self._generate(z, y, is_training=False)
-        # tf.summary.image('xh', tf.transpose(xh, [2, 1, 0, 3]))
-        # return self._filter(xh, is_training=False)
         return xh
-
-    # def classify(self, x):
-    #     return self._classify(tf.nn.softmax(x), is_training=False)
-
-    # def interpolate(self, x1, x2, n):
-    #     ''' Interpolation from the latent space '''
-    #     x1 = tf.expand_dims(x1, 0)
-    #     x2 = tf.expand_dims(x2, 0)
-    #     z1, _ = self._encode(x1, is_training=False)
-    #     z2, _ = self._encode(x2, is_training=False)
-    #     a = tf.reshape(tf.linspace(0., 1., n), [n, 1])
-
-    #     z1 = tf.matmul(1. - a, z1)
-    #     z2 = tf.matmul(a, z2)
-    #     z = tf.nn.tanh(tf.add(z1, z2))  # Gaussian-to-Uniform
-    #     xh = self._generate(z, is_training=False)
-    #     xh = tf.concat(0, [x1, xh, x2])
-    #     return xh
